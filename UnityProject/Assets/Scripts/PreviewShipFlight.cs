3512b335590d1e2e34dffcb2f766b90eb637343b
Kit Burgess <kburgess@gamedesignkit.net>
2021-01-27 08:13:24 -0700

    Added Spline Editor Asset; WIP planning of moves;

---
+++ UnityProject/Assets/Scripts/PreviewShipFlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BansheeGz.BGSpline.Curve;
+
+public class PreviewShipFlight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    BGCurve curve;
+
+    void Start()
+    {
+        curve = gameObject.GetComponent<BGCurve>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //curve.Fields.
+    }
+}
---
+++ UnityProject/Assets/Scripts/ScriptableObjects/ShipStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ship", menuName = "ScriptableObjects/Ship", order = 1)]
+public class ShipStats : ScriptableObject
+{
+    public int numThrusters = 3;
+    public int numVectorThrusters = 2;
+
+    public int TopSpeed { get {
+        return numThrusters;
+    }}
+}
