2f56b2e4df2a370c4af3e7bd2a11753ff1226588
Kit Burgess <kburgess@gamedesignkit.net>
2021-01-30 15:18:04 -0700

    Exhaust trail particles;

---
+++ UnityProject/Assets/Scripts/ExhaustTrailAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExhaustTrailAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    ParticleSystem exhaustParticles;
+
+    void Start()
+    {
+        exhaustParticles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TimeManager.TurnDeltaTime > 0) {
+            exhaustParticles.Simulate(TimeManager.TurnDeltaTime, true, false);
+        }
+    }
+}
