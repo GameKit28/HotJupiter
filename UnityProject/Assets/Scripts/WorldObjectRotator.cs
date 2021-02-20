32396827cfebef042cd76da8ba3676f21825c3dd
Kit Burgess <kburgess@gamedesignkit.net>
2021-02-20 16:55:17 -0700

    Moved third-party assets to 3rdParty folder;

---
+++ UnityProject/Assets/Scripts/WorldObjectRotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class WorldObjectRotator : MonoBehaviour
+{
+    const float minRotationRate = 15f;
+    const float maxRotationRate = 5f;
+
+    Vector3 rotationPole;
+    float rotationRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationPole = Random.insideUnitSphere;
+        rotationRate = Random.Range(minRotationRate, maxRotationRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationPole, rotationRate * TimeManager.TurnDeltaTime, Space.Self);
+    }
+}
